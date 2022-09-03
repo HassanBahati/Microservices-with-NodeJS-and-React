d07e8ee3ea6d6345291675528e4f2efe51df732f
HassanBahati <mukisabahati@gmail.com>
2022-09-03 13:06:49 +0300

    set up storage for the posts

--- postsService/index.js
+++ postsService/index.js
@@ -4,11 +4,18 @@ const express = require("express");
 // create new app as instance of express
 const app = express();
 
+// store all posts that get created
+const posts = {};
+
 // get request
-app.get("/posts", (req, res) => {});
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
 //post request
-app.post("/posts", (req, res) => {});
+app.post("/posts", (req, res) => {
+    
+});
 
 // app to listen on port 4000
 app.listen(4000, () => {
