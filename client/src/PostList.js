a0917e7a30e59fa9e0ab8eb5086d1277082f58eb
HassanBahati <mukisabahati@gmail.com>
2022-09-03 17:14:04 +0300

    rendercomments under post

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import CommentCreate from "./CommentCreate";
 
 const PostList = () => {
   const [posts, setPosts] = useState({});
@@ -22,6 +23,7 @@ const PostList = () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentCreate postId={post.id}/>
         </div>
       </div>
     );
