9017a270ac0b5df948c4c9bf295567d31d8267ea
HassanBahati <mukisabahati@gmail.com>
2022-09-03 16:55:51 +0300

    unctinoality to fetch posts

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const PostList = () => {
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    const res = await axios.get("http:localhost:4000/posts");
+    setPosts(res.data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return <div></div>;
 };
 
