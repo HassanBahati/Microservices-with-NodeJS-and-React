e7cf47149d73a22e56370412bb6871a962e09686
HassanBahati <mukisabahati@gmail.com>
2022-09-03 16:51:42 +0300

    render post list

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 export default () => {
   return (
     <div className="container my-5">
       <h1>Create Post</h1>
       <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 };
