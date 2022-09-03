6f886b832445c2acb02f1d0700eb43c56a621a45
HassanBahati <mukisabahati@gmail.com>
2022-09-03 17:27:10 +0300

    fetch comments

---
+++ client/src/CommentList.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const CommentList = ({ postId }) => {
+  const [comments, setComments] = useState([]);
+
+  const fetchComments = async () => {
+    const res = await axios.get(`http://localhost/posts/${postId}/comment`);
+
+    setComments(res.data);
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
