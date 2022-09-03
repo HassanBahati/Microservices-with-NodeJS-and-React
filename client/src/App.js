2f41acc13eea32c3341043c8dd0a84e07d6ef981
HassanBahati <mukisabahati@gmail.com>
2022-09-03 15:59:48 +0300

    render post create component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
-import React from 'react'
-
-export default () =>{
-    return <div>Post app</div>
-}
+import React from "react";
+import PostCreate from "./PostCreate";
+export default () => {
+  return (
+    <div>
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
+};
