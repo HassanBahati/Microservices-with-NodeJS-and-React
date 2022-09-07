6aaa5393d626af05c4078eecf2d7d40cd091ce6c
HassanBahati <mukisabahati@gmail.com>
2022-09-07 19:26:34 +0300

    set up server

---
+++ moderationService/index.js
@@ -0,0 +1,22 @@
+// import  modules
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// create new app as instance of express
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/events", (req, res) => {
+  
+
+  console.log(posts)
+  //send status sucessful
+  res.send({ status: "OK" });
+});
+
+// app to listen on port 4003
+app.listen(4003, () => {
+  console.log("moderation service listening on 4003");
+});
