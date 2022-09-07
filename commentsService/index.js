354f851be572f874f2efee6a88e8926d3fa448cb
HassanBahati <mukisabahati@gmail.com>
2022-09-07 22:42:00 +0300

    fix bug

--- commentsService/index.js
+++ commentsService/index.js
@@ -50,7 +50,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 app.post("/events", async (req, res) => {
   console.log("Received event:", req.body.type);
 
-  const { type, data } = req.status;
+  const { type, data } = req.body;
 
   if (type === "CommentModerated") {
     const { postId, id, status } = data;
