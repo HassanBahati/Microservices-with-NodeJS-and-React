98b264b6966acb616459f5960d450d2f402f9b2b
HassanBahati <mukisabahati@gmail.com>
2022-09-08 09:29:27 +0300

    handle statstuses

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -16,7 +16,7 @@ const CommentList = ({ comments }) => {
       content = "This comment has been rejected.";
     }
 
-    return <li key={comment.id}>{comment.content}</li>;
+    return <li key={comment.id}>{content}</li>;
   });
   return <ul>{renderedComments}</ul>;
 };
