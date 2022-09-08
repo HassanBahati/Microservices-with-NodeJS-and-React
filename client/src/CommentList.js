c46ee416b50cee36e1d9c94f4935e9e72e90e7b6
HassanBahati <mukisabahati@gmail.com>
2022-09-08 09:29:21 +0300

    handle statstuses

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,6 +2,20 @@ import React from "react";
 
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
+    let content;
+
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation.";
+    }
+
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected.";
+    }
+
     return <li key={comment.id}>{comment.content}</li>;
   });
   return <ul>{renderedComments}</ul>;
